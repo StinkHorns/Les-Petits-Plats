fb64c84ef91ef36cc05dafa5e2bd86a6334c4963
StinkHorns <marcodelolmo@hotmail.com>
2024-05-26 14:02:40 +0200

    Search Boucle Native 1

--- js_files/index.js
+++ js_files/index.js
@@ -1,5 +1,8 @@
 // retreive Json Data Recettes
 
+
+const inputValue= document.getElementById('search-focus');
+let dataFromRecettes = []
 async function getRecettesData (){
 
     let data = await fetch ("./data/recipes.json");
@@ -10,26 +13,25 @@ async function getRecettesData (){
     return dataRecettes;
 }
  
+
 async function init(){        
     
     console.log("hola2");
 
-    let dataFromRecettes = await getRecettesData();
+     dataFromRecettes = await getRecettesData();
     console.log(dataFromRecettes.length + " hola");
-    const cardContainer = document.getElementById('cardContainer');
+    
+    displayRecipes(dataFromRecettes)
 
-    dataFromRecettes.forEach((recetteSolo)=>{
-        const recetteCard = dataForCard (recetteSolo);
-        const cardDOM = recetteCard.createCard();
-        cardContainer.appendChild (cardDOM);
 
 
     
-    dataForFiltres(recetteSolo);
+   
 
 
-        }
-        )
+        
+        
+        
 // Recettes Total Counter
     const recetteTotales = document.createElement('div');
     recetteTotales.classList.add('countRecettes');
@@ -38,6 +40,52 @@ async function init(){
 
 }
 
+function displayRecipes(datas){
+    const cardContainer = document.getElementById('cardContainer');
+    cardContainer.innerHTML ="";
+        datas.forEach((recetteSolo)=>{
+            const recetteCard = dataForCard (recetteSolo);
+            const cardDOM = recetteCard.createCard();
+            cardContainer.appendChild (cardDOM);
+            
+        dataForFiltres(recetteSolo);
+        })
+}
+
+
+inputValue.addEventListener('input', ()=>{
+    search(inputValue.value, dataFromRecettes)
+})
+
+
+function search(searchInput, datas){
+    // verifier la taile de la chaine 
+     let dataResult = [];
+    if(searchInput.length >=3){
+        // on lance la recherche
+
+    for(let i =0; i<= datas.length-1; i++){
+        if(datas[i].name.toLowerCase().includes(searchInput.toLowerCase())){
+            dataResult.push(datas[i])
+        }
+         if(datas[i].description.toLowerCase().includes(searchInput.toLowerCase())){
+            dataResult.push(datas[i])
+        }
+
+        for(let y =0; y<=datas[i].ingredients.length-1; y++){
+           if(datas[i].ingredients[y].ingredient.includes(searchInput)){
+               dataResult.push(datas[i])
+            }
+        }
+
+    }
+
+    displayRecipes(dataResult)
+
+    }
+
+
+}
     
 init();
 
